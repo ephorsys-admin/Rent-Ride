979ed8e6ff26ca8932dad9229bd1bae926664b6a
Happy Samal <rudrasamal007@gmail.com>
2026-02-15 16:05:34 +0530

    solve build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import Router from './routes/Router';
 import './App.css';
-import useScrollAnimation from './Shared/Helpers/UseScrollAnimation';
+import useScrollAnimation from './Shared/Helpers/useScrollAnimation';
 
 function App() {
    useScrollAnimation();
