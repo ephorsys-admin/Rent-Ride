7aad274c9fff51df199283bf66152b0df06bd0fc
bisworanjanEphorsys2004 <bisworanjan.ephorsys@gmail.com>
2026-05-20 11:45:26 +0530

    last changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    compress: true,
-    minify: 'terser',
+    minify: 'esbuild',
     sourcemap: false,
     rollupOptions: {
       output: {
